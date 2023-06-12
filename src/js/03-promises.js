a732161601bb430fcf3024d0f974e109aaa4a847
Yuriy-St <yuriystaynov@gmail.com>
2023-06-13 00:20:23 +0300

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
+'use strict';
+
+import { Notify } from 'notiflix';
+import 'notiflix/dist/notiflix-3.2.6.min.css';
+
+const formEl = document.querySelector('.form');
+const delayEl = formEl.elements.delay;
+const stepEl = formEl.elements.step;
+const amountEl = formEl.elements.amount;
+const btnSubmitEl = formEl.querySelector('button');
+
+btnSubmitEl.addEventListener('click', evt => {
+  evt.preventDefault();
+  let delay = +delayEl.value;
+  const step = +stepEl.value;
+  const amount = +amountEl.value;
+
+  for (let i = 1; i <= amount; i++) {
+    setTimeout(
+      delay => {
+        createPromise(i, delay)
+          .then(msgFullfilled => {
+            Notify.success(msgFullfilled);
+          })
+          .catch(msgRejected => {
+            Notify.failure(msgRejected);
+          });
+      },
+      delay,
+      delay
+    );
+    delay += step;
+  }
+});
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    return Promise.resolve(`✅ Fulfilled promise ${position} in ${delay} ms`);
   } else {
-    // Reject
+    return Promise.reject(`❌ Rejected promise ${position} in ${delay}ms`);
   }
 }
